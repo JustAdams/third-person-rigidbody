89627ac8494f6a0c49a868ee750f0ec69c3a235b
Justin Adams <adamsjustin@protonmail.com>
2020-10-05 23:00:47 -0400

    Initial commit

---
+++ Third-person Rigidbody/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+    PlayerController pc;
+
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pc = GetComponent<PlayerController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * speed;
+        pc.Move(moveVelocity);
+    }
+}
---
+++ Third-person Rigidbody/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    Vector3 velocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + velocity * Time.fixedDeltaTime);
+    }
+
+    public void Move(Vector3 movementVelocity)
+    {
+        velocity = movementVelocity;
+    }
+
+}
